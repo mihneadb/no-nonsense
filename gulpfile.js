94b568f2625ef71bc916f4ea06b8a2edbcfeec6e
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2014-07-27 16:05:10 +0300

    Use gulp to minify css in order.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,13 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var del = require('del');
+var minifyCSS = require('gulp-minify-css');
+var streamqueue  = require('streamqueue');
+
 
 var paths = {
-    scripts: ['assets/js/**/*.js']
+    scripts: ['assets/js/**/*.js'],
+    css: ['assets/css/**/*.css']
 };
 
 // Not all tasks need to use streams
@@ -17,16 +21,35 @@ gulp.task('clean', function(cb) {
 
 gulp.task('scripts', ['clean'], function() {
     // Minify and copy all JavaScript
-    return gulp.src(paths.scripts)
+    return streamqueue({objectMode: true},
+        gulp.src('assets/js/jquery.fitvids.js'),
+        gulp.src('assets/js/prism.js'),
+        gulp.src('assets/js/instantclick.min.js'),
+        gulp.src('assets/js/theme.js')
+    )
         .pipe(uglify())
         .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('assets/build'));
+});
+
+gulp.task('minify-css', function() {
+    return streamqueue({ objectMode: true },
+        gulp.src('assets/css/normalize.css'),
+        gulp.src('assets/css/foundation.css'),
+        gulp.src('assets/css/fontawesome.css'),
+        gulp.src('assets/css/prism.css'),
+        gulp.src('assets/css/style.css')
+    )
+        .pipe(minifyCSS())
+        .pipe(concat('all.min.css'))
         .pipe(gulp.dest('assets/build'));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
     gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.css, ['minify-css']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'scripts', 'minify-css']);
